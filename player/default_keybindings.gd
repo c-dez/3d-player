ec295a0d1ab576aff20878d275729dc3d0e19bc1
c-dez <c-dez@proton.me>
2026-07-26 20:54:39 -0600

    anadido s nodo temporal que asigna los keybindings a Engine para drop and play sin tener que configurarlos antes en engine

---
+++ player/default_keybindings.gd
@@ -0,0 +1,41 @@
+extends Node
+
+## Usado para asignar las teclas de movimiento y salto por default, borrar este nodo cuando se quieran asignar desde Egine
+
+# var keybindings :Dictionary = {
+#     "LEFT" : 'left'
+# }
+var keybindings: Array = [
+    'left',
+    'right',
+    'up',
+    'down',
+    'jump'
+]
+
+func _ready() -> void:
+    set_keybinding(keybindings)
+
+
+func set_keybinding(keys: Array) -> void:
+    for key in keys:
+        InputMap.add_action(key)
+        var evento = InputEventKey.new()
+        match key:
+            'left':
+                evento.keycode = KEY_A
+            'right':
+                evento.keycode = KEY_D
+            'up':
+                evento.keycode = KEY_W
+            'down':
+                evento.keycode = KEY_S
+            'jump':
+                evento.keycode = KEY_SPACE
+
+
+
+        InputMap.action_add_event(key, evento)
+
+                
+        pass
